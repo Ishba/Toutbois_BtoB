9b9e099bcdd469cd1e1081ad80c86bb23b9f5bef
Arnaud Herbez <arnaudh59@gmail.com>
2017-11-03 08:39:47 +0100

    Creation_pages_depart

---
+++ global/haut.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title></title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/styles.css">
+  </head>
+
+  <body>
+    <div class="container">
