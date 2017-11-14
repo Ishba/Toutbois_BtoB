59048a431a2092b0bd125b7e2da78097615146ff
Arnaud Herbez <arnaudh59@gmail.com>
2017-11-14 15:47:52 +0100

    debut save commande

---
+++ public/addcommande.php
@@ -0,0 +1,19 @@
+<?php
+  require('../view/header.php');
+/*  $json = array('error' => true);
+  if(isset($_GET['id'])) {
+    $produit = $DB->query('SELECT id_produit FROM produit WHERE id_produit=:id', array('id' => $_GET['id']));
+    if(empty($produit)) {
+      $json['message'] = "Ce produit n'existe pas !";
+    }
+    $panier->add($produit[0]->id_produit);
+    $json['error'] = false;
+    $json['total'] = $panier->total();
+    $json['count'] = $panier->count();
+    $json['message'] = 'Produit ajouté au panier !';
+  } else {
+    $json['message'] = "Vous n'avez pas selectionné de produit à ajouter au panier !";
+  }
+  echo json_encode($json);*/
+
+ ?>
